8fa9ba7d69c5b746c6f14bb413d7907501888fb5
Fernando Sanchez <fesp321@gmail.com>
2024-10-31 14:55:22 -0600

    Added Dockerfile and docker-compose

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,7 @@ import Resizer from '@/components/sections/Resizer';
 import Gallery from '@/components/sections/Gallery';
 import Header from '@/components/sections/Header';
 import Section from '@/components/common/Section';
+import { GalleryProvider } from '@/context/GalleryContext';
 import { ToastContainer } from 'react-toastify';
 
 export default function HomePage() {
@@ -12,12 +13,14 @@ export default function HomePage() {
       <Section height="1/2">
         <Header />
       </Section>
-      <Section height="1/2">
-        <Resizer />
-      </Section>
-      <Section>
-        <Gallery />
-      </Section>
+      <GalleryProvider>
+        <Section height="1/2">
+          <Resizer />
+        </Section>
+        <Section>
+          <Gallery />
+        </Section>
+      </GalleryProvider>
       <ToastContainer />
     </>
   );
